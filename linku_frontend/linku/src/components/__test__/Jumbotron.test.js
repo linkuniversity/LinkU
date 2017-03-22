99e0e6d704c59f18f6f71b2d97a377f82fd6f897
jang <wkdtjsgur100@naver.com>
2017-03-22 20:50:10 +0900

    Changed architecture to redux

--- linku_frontend/linku/src/components/__test__/Jumbotron.test.js
+++ linku_frontend/linku/src/components/__test__/Jumbotron.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {shallow} from 'enzyme';
-import Jumbotron from '../src/Jumbotron';
+import Jumbotron from '../mainpage/Jumbotron';
 
 describe('<Jumbotron />', () => {
     it( 'renders without exploding', () => {
