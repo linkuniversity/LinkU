862ec5883b6e7a9196f36c7f4fbf0a06514135a0
cjh5414 <cjh5414@gmail.com>
2017-03-27 21:46:46 +0900

    Add id field on MeetingSerializer

--- linku_backend/meeting/serializer.py
+++ linku_backend/meeting/serializer.py
@@ -7,5 +7,5 @@ class MeetingSerializer(serializers.HyperlinkedModelSerializer):
 
     class Meta:
         model = Meeting
-        fields = ('maker_name', 'title', 'start_time', 'main_image', 'place', 'price', 'num_of_joined_members',
+        fields = ('id', 'maker_name', 'title', 'start_time', 'main_image', 'place', 'price', 'num_of_joined_members',
                   'max_num_of_members', 'meeting_specific_info', 'restaurant_name', 'category', 'specific_link', 'appliers')
