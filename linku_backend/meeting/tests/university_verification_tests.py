570e7bf41e8eef853b0469a2caa20882f5622f0d
TWpower <elld8850@naver.com>
2017-04-14 03:06:45 +0900

    Remove university_verification_tests.py

--- linku_backend/meeting/tests/university_verification_tests.py
+++
@@ -1,9 +0,0 @@
-import json
-
-
-def test_read_correct_mail_conf_file():
-    with open('mail_setting.json') as data_file:
-        mail_setting = json.load(data_file)
-
-        assert mail_setting['email'] == 'team.uniculture@gmail.com'
-        assert mail_setting['password'] is not None
