d105573f177cbed1f5b317d51df4bc5b8ae0a3cd
TWpower <elld8850@naver.com>
2017-04-14 12:12:12 +0900

    Add encoding code to views.py

--- linku_backend/meeting/views.py
+++ linku_backend/meeting/views.py
@@ -1,3 +1,5 @@
+#-*- coding: utf-8 -*-
+
 from rest_framework import viewsets
 from rest_framework.decorators import api_view
 from .serializer import MeetingSerializer, UserSerializer, SubImageSerializer
