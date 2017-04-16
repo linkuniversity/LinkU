e849840931b2498ec08dcdd5d4b0f5374ab07ac2
TWpower <elld8850@naver.com>
2017-04-16 15:07:42 +0900

    Add header,movie,introduction-ui

--- linku_frontend/linku/src/components/App.js
+++ linku_frontend/linku/src/components/App.js
@@ -10,15 +10,17 @@ import Signup from './signup/Signup';
 import Login from './login/Login';
 import Jumbotron from './mainpage/Jumbotron';
 import MeetingCardBox from './mainpage/MeetingCardBox';
+import IntroMovie from './mainpage/IntroMovie';
+import IntroOfLinkU from './mainpage/IntroOfLinkU';
+import LinkUHeader from './mainpage/LinkUHeader';
 
 export default class App extends Component {
     render() {
         return (
             <div>
-                <Signup />
-                <Login />
-                <Jumbotron />
-                <MeetingCardBox />
+                <LinkUHeader />
+                <IntroMovie />
+                <IntroOfLinkU />
             </div>
         );
     }
