cf6de5141afd8cb486f5663a0f7b715ff08c0790
cjh5414 <cjh5414@gmail.com>
2017-04-16 16:34:38 +0900

    Setup functional test with selenium

---
+++ linku_backend/functional_tests/authentication_tests.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+import sys
+import os
+
+from selenium import webdriver
+import pytest
+
+BASE_URL = "http://localhost:3000"
+
+
+@pytest.fixture(scope="module")
+def browser():
+    if sys.platform == 'darwin':
+        project_root = os.path.dirname(os.path.dirname(
+            os.path.realpath(__file__)))
+        repo_root = os.path.dirname(project_root)
+        sys.path.append(os.path.join(repo_root, 'dev'))
+        import download_chromedriver
+        download_chromedriver.download()
+        chrome_path = download_chromedriver.get_chromedriver_path()
+        if chrome_path is False:
+            raise SystemExit
+        driver = webdriver.Chrome(chrome_path)
+    else:
+        driver = webdriver.Firefox()
+    yield driver
+    driver.close()
+
+
+def test_first_page_cards_title(browser):
+    browser.get(BASE_URL)
+    assert 'LinkU' in browser.title
+
