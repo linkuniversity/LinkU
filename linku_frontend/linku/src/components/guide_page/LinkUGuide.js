cef55cfb13d3a097ebaf32546c085d2f7ce47767
TWpower <elld8850@naver.com>
2017-04-16 21:00:45 +0900

    Add LinkUGuide Style

--- linku_frontend/linku/src/components/guide_page/LinkUGuide.js
+++ linku_frontend/linku/src/components/guide_page/LinkUGuide.js
@@ -8,18 +8,77 @@ class LinkUGuide extends React.Component{
     }
 
     render(){
-        return(
-            <Container>
-                <Grid centered>
-                    <div>
-                    <Image src="http://localhost:8000/media/meeting_default_image.jpg" />
-                    </div>
 
-                    <div>
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                    </div>
-                </Grid>
-            </Container>
+        let descriptionStyle = {
+            marginTop: "2%",
+        }
+
+        let guideImageStyle = {
+            width: "350px",
+            height: "350px",
+        }
+
+        let containerStyle = {
+            margin: "5%",
+        }
+
+        return(
+            <div>
+                <Container style={containerStyle}>
+                    <Grid>
+                        <div>
+                            <div>
+                                <Image style={guideImageStyle} src="http://localhost:8000/media/meeting_default_image.jpg" floated="left"/>
+                            </div>
+
+                            <div style={descriptionStyle}>
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                            </div>
+                        </div>
+                    </Grid>
+                </Container>
+
+                <Container style={containerStyle}>
+                    <Grid>
+                        <div>
+                            <div>
+                                <Image style={guideImageStyle} src="http://localhost:8000/media/meeting_default_image.jpg" floated="right"/>
+                            </div>
+
+                            <div style={descriptionStyle}>
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                            </div>
+                        </div>
+                    </Grid>
+                </Container>
+
+                <Container style={containerStyle}>
+                    <Grid>
+                        <div>
+                            <div>
+                                <Image style={guideImageStyle} src="http://localhost:8000/media/meeting_default_image.jpg" floated="left"/>
+                            </div>
+
+                            <div style={descriptionStyle}>
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                            </div>
+                        </div>
+                    </Grid>
+                </Container>
+
+                <Container style={containerStyle}>
+                    <Grid>
+                        <div>
+                            <div>
+                                <Image style={guideImageStyle} src="http://localhost:8000/media/meeting_default_image.jpg" floated="right"/>
+                            </div>
+                            <div style={descriptionStyle}>
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                            </div>
+                        </div>
+                    </Grid>
+                </Container>
+            </div>
         );
     }
 
