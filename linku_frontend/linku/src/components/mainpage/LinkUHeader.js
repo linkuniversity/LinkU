3958fcdaa6ed3996d9915e33638fd50025dada08
cjh5414 <cjh5414@gmail.com>
2017-04-17 20:25:17 +0900

    Close singup modal after compeleted signup

--- linku_frontend/linku/src/components/mainpage/LinkUHeader.js
+++ linku_frontend/linku/src/components/mainpage/LinkUHeader.js
@@ -1,15 +1,17 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {Container, Image, Button} from 'semantic-ui-react'
 import Signup from '../signup/Signup.js'
 import Login from '../login/Login.js'
 
-const LinkUHeader = () => (
-    <Container>
-        <Image src='http://localhost:8000/media/logo_top.png' verticalAlign='top'/>
-        <span>Link U Link University</span>
-        <Signup triggerButton={<Button basic content="회원가입" floated="right"/>}/>
-        <Login triggerButton={<Button basic content="로그인" floated="right"/>}/>
-    </Container>
-);
-
-export default LinkUHeader;
+export default class LinkUHeader extends Component {
+    render() {
+        return (
+            <Container>
+                <Image src='http://localhost:8000/media/logo_top.png' verticalAlign='top'/>
+                <span>Link U Link University</span>
+                <Signup />
+                <Login triggerButton={<Button basic content="로그인" floated="right"/>}/>
+            </Container>
+        );
+    }
+}
--- linku_frontend/linku/src/components/signup/Signup.js
+++ linku_frontend/linku/src/components/signup/Signup.js
@@ -10,6 +10,16 @@ import SignupForm from './SignupForm';
 import axios from 'axios';
 
 class Signup extends Component {
+    state = { modalOpen: false }
+
+    handleOpen = (e) => this.setState({
+        modalOpen: true,
+    })
+
+    handleClose = (e) => this.setState({
+        modalOpen: false,
+    })
+
     _handleSignupSubmit = async (values) => {
         if( values.password != values.password_check )
             console.log("password is not equal");
@@ -17,6 +27,7 @@ class Signup extends Component {
             const info = await Promise.all([axios.post('http://127.0.0.1:8000/users/',values)
                 .then(response => {
                     this.props.alertConfirm("회원가입이 완료되었습니다.", "blue");
+                    this.handleClose();
                     console.log(response.data);
                 })
                 .catch(error => {
@@ -29,7 +40,10 @@ class Signup extends Component {
 
     render() {
         return (
-            <Modal trigger={this.props.triggerButton} size='small'>
+            <Modal trigger={<Button onClick={this.handleOpen} basic content="회원가입" floated="right"/>}
+                open={this.state.modalOpen}
+                onClose={this.handleClose}
+                size='small'>
                 <Modal.Header>링쿠 회원가입</Modal.Header>
                 <Modal.Content>
                     <Modal.Description>
