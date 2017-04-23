ac45eb8562aad7ad9beed1117ce532f7a3fffd2b
cjh5414 <cjh5414@gmail.com>
2017-04-24 02:01:39 +0900

    Refactor get date's string in frontend

--- linku_frontend/linku/src/components/mainpage/MeetingCard.js
+++ linku_frontend/linku/src/components/mainpage/MeetingCard.js
@@ -40,6 +40,12 @@ class MeetingCard extends React.Component
         localStorage.setItem('participated_dates', undefined);
     }
 
+    getDateStr(date) {
+        const WEEK_DAY = ["일", "월", "화", "수", "목", "금", "토"];
+        const meeting_date = new Date(date);
+        return (meeting_date.getMonth() + 1)+ "월 " + meeting_date.getDate() + "일 " + WEEK_DAY[meeting_date.getDay()] + "요일";
+    }
+
     render() {
         const statisticsNumberStyle = {
             color : '#FFFFFF',
@@ -114,10 +120,7 @@ class MeetingCard extends React.Component
         if(this.props.meetingInfo.status_by_days)
         {
             meetingDateOptions = this.props.meetingInfo.status_by_days.map((status, index) => {
-                const WEEK_DAY = ["일", "월", "화", "수", "목", "금", "토"];
-                const meeting_date = new Date(status.start_time);
-                const meeting_date_str = (meeting_date.getMonth() + 1)+ "월 " + meeting_date.getDate() + "일 " + WEEK_DAY[meeting_date.getDay()] + "요일";
-                const button_message = meeting_date_str + " (" + (status.participant_num.man + status.participant_num.woman)
+                const button_message = this.getDateStr(status.start_time) + " (" + (status.participant_num.man + status.participant_num.woman)
                                         + "/" + status.max_num_of_members + ")명";
 
                 return { key: index, text: button_message, value: index };
@@ -132,12 +135,9 @@ class MeetingCard extends React.Component
                 return;
 
             const selected_meeting = this.props.meetingInfo.status_by_days[this.state.selectedValue];
-            const WEEK_DAY = ["일", "월", "화", "수", "목", "금", "토"];
-            const selected_date = new Date(selected_meeting.start_time);
-            const selected_date_str = (selected_date.getMonth() + 1)+ "월 " + selected_date.getDate() + "일 " + WEEK_DAY[selected_date.getDay()] + "요일";
             const user_gender = localStorage.getItem('user_gender');
             let participant_num_by_gender = undefined;
-            
+
             if(localStorage.getItem('user_gender')=='F')
                 participant_num_by_gender = this.state.participant_woman_num;
             else
@@ -156,7 +156,7 @@ class MeetingCard extends React.Component
                     return (
                         <Apply
                             selectedValue={this.state.selectedValue}
-                            paymentInfo={selected_date_str}
+                            paymentInfo={this.getDateStr(selected_meeting.start_time)}
                         />
                     );
                 }
