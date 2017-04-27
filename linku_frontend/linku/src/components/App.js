1495e76fc889822d20f8f8db2eba79c2954da885
TWpower <elld8850@naver.com>
2017-04-27 19:53:12 +0900

    Remove NextMeetingPlan

--- linku_frontend/linku/src/components/App.js
+++ linku_frontend/linku/src/components/App.js
@@ -25,7 +25,6 @@ const App = () => (
         <LinkUHeader />
         <IntroOfLinkU />
         <MeetingCardBox />
-        <NextMeetingPlan />
         <Review />
         <Statistics />
         <LinkUContact />
