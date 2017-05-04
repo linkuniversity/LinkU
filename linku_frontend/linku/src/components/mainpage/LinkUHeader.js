9150790cd89d50d5377e989fd8c0bdd9b65439d3
cjh5414 <cjh5414@gmail.com>
2017-05-04 11:58:43 +0900

    Add login, logout button in HeaderBar

--- linku_frontend/linku/src/components/mainpage/LinkUHeader.js
+++ linku_frontend/linku/src/components/mainpage/LinkUHeader.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
-import {Container, Image, Button, Modal} from 'semantic-ui-react'
-import Signup from '../signup/Signup.js'
+import { Image, Button } from 'semantic-ui-react'
 import Login from '../login/Login.js'
 import { connect } from 'react-redux';
 import * as actions from '../../actions/Login';
@@ -9,63 +8,20 @@ import { bindActionCreators } from 'redux';
 import {DEFAULT_REQUEST_URL} from '../utils/RequestUrlSetting';
 
 class LinkUHeader extends Component {
-    constructor(props) {
-        super(props);
-    }
-
     render() {
         let containerStyle = {
-            marginTop: "40px",
-        };
-
-        let titleStyle = {
-            fontFamily: '../res/assets/KoPubDotumMedium.ttf',
-            fontSize: '14pt',
-            marginLeft: '12px',
-            color: '#60a2d9'
-        };
-
-        let signWrapperStyle = {
-            float: 'right',
-        };
-
-        let leftButtonStyle = {
-            fontFamily: '../res/assets/KoPubDotumMedium.ttf',
-            fontSize: '14pt',
-            color: '#5fa1d7',
-            backgroundColor: '#FFFFFF',
-            border: '0px'
-        };
-
-        let rightButtonStyle={
-            marginLeft: '10px',
-            fontFamily: '../res/assets/KoPubDotumMedium.ttf',
-            fontSize: '14pt',
-            color: '#5fa1d7',
-            backgroundColor: '#FFFFFF',
-            border: '0px'
-        };
-
-        let signOutButtonStyle={
-            float: 'right',
-            fontFamily: '../res/assets/KoPubDotumMedium.ttf',
-            fontSize: '14pt',
-            color: '#5fa1d7',
-            backgroundColor: '#FFFFFF',
-            border: '0px'
+            padding: '5px'
         };
 
         return (
-            <Container text textAlign="center" style={containerStyle}>
-                <div style={{textAlign: "center"}}>
-                    <Image src={DEFAULT_REQUEST_URL+'/media/logo_top.png'} verticalAlign='top'/>
-                    <span style={titleStyle}>Link U Link University</span>
-                </div>
-                <p style={{marginTop: '15px', fontSize:'13px',color: '#60a2d9'}}>
-                    링쿠는 현재 베타 서비스입니다. <br/>
-                    본 서비스에서 더 좋은 모습 보여드릴게요
-                </p>
-            </Container>
+            <div style={containerStyle}>
+                <Image src={DEFAULT_REQUEST_URL+'/media/logo_top.png'} verticalAlign='top'/>
+                {
+                    (localStorage.getItem('token') && this.props.loggedIn) ?
+                    (<Button onClick={this.props.logout} basic color='blue' floated='right'>로그아웃</Button>) :
+                    (<Login triggerButton={<Button basic color='blue' floated='right'>로그인</Button>}/>)
+                }
+            </div>
         );
     }
 }
