11cdc49d473f66e82ae92455fe95f3ae548e504e
TWpower <elld8850@naver.com>
2017-05-17 18:28:17 +0900

    Add ga setting to SignUp Selection page and add guide text

--- linku_frontend/linku/src/components/signup/SignupSelection.js
+++ linku_frontend/linku/src/components/signup/SignupSelection.js
@@ -15,12 +15,22 @@ class SignupSelection extends Component {
         super(props);
     }
 
+    componentDidMount() {
+        if (process.env.REACT_APP_LINKU_SERVER_ENVIRONMENT === 'production'){
+            var ReactGA = require('react-ga');
+            ReactGA.pageview(window.location.pathname);
+        }
+    }
 
     render(){
         return (
             <Container text>
                 <Button style={buttonStyle} onClick={() => {this.props.history.push('/signup')}}  fluid>학교메일로 인증하기</Button>
                 <Button style={buttonStyle} href="http://plus.kakao.com/home/@linku" fluid>학생증으로 인증하기</Button>
+                <p style={{fontSize: "17px", color: "#60a2d9", textAlign: "center"}}>
+                    ※ 대학생 이메일로 인증이 어려우신 분은 "학생증으로 인증하기"를 통해 <br/>
+                    학생증 사진을 보내주시는 것으로도 가입이 가능해요 :D
+                </p>
             </Container>
         );
     }
