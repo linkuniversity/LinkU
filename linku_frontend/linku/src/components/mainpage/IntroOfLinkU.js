a76f2784b9a3d9ca9eead06e01cb1305ca7c2b3b
jang <wkdtjsgur100@naver.com>
2017-05-19 11:28:54 +0900

    Add login & logout button on header

--- linku_frontend/linku/src/components/mainpage/IntroOfLinkU.js
+++ linku_frontend/linku/src/components/mainpage/IntroOfLinkU.js
@@ -6,6 +6,8 @@ import {Link} from 'react-router-dom';
 import LinkUGuide from '../guide_page/LinkUGuide';
 import {DEFAULT_REQUEST_URL} from '../utils/RequestUrlSetting';
 import {withRouter} from 'react-router-dom';
+import {connect} from 'react-redux';
+import {logout} from '../../actions/Login';
 
 class IntroOfLinkU extends React.Component {
     constructor(props) {
@@ -21,17 +23,46 @@ class IntroOfLinkU extends React.Component {
       modalOpen: false,
     })
 
+    _getButtonByTokenState = () => {
+        let loginAndOutStyle = {
+            fontSize: '17px',
+            lineHeight: '40px',
+            textShadow: '0.0em 0.1em 0.15em #666',
+            color:"#FFFFFF",
+            backgroundColor: "transparent"
+        }
+
+        if(this.props.loggedIn){
+            return (<Button
+                style={loginAndOutStyle}
+                onClick={() => {
+                    this.props.logout();
+                }}>
+                    로그아웃
+                </Button>);
+        }
+        else{
+            return (<Button
+                style={loginAndOutStyle}
+                onClick={() => {
+                    this.props.history.push('/login');
+                }}>
+                    로그인
+                </Button>);
+        }
+    }
+
     render() {
         let MediaQuery = require('react-responsive');
         let containerStyle = {
-            paddingTop: '100px',
+            paddingTop: '50px',
             textAlign: 'center',
             backgroundImage: 'url('+ DEFAULT_REQUEST_URL +'/media/header_background.png)',
             backgroundRepeat: 'no-repeat',
             backgroundPosition: 'center top',
             backgroundAttachment: 'fixed',
             width: '100%',
-            height: '480px',
+            height: '560px',
             color: '#FFFFFF',
         }
 
@@ -60,9 +91,11 @@ class IntroOfLinkU extends React.Component {
             color: '#FFFFFF',
             fontSize: '26px'
         }
-
         return (
             <Container centered style={containerStyle}>
+                <Container style={{marginBottom:"30px"}} textAlign="right">
+                    {this._getButtonByTokenState()}
+                </Container>
                 <Image src={DEFAULT_REQUEST_URL+"/media/logo_top.png"} centered />
                 <MediaQuery minDeviceWidth={1}>
                     <MediaQuery minWidth={600}>
@@ -70,7 +103,7 @@ class IntroOfLinkU extends React.Component {
                             <b>링쿠</b>는 다같이 <b>먹고 놀고 친해지는</b> <br/>
                             <b>대학생 밥모임</b> 연결 서비스입니다. <br/>
                         </div>
-                        <Button style={{backgroundColor: '#5fa1d7', width: '150px', fontSize: '16px'}} color='blue' content='이용안내'
+                        <Button style={{backgroundColor: '#5fa1d7', marginTop: '30px', width: '150px', fontSize: '16px'}} color='blue' content='이용안내'
                             onClick={() => this.props.history.push('/about')} />
                     </MediaQuery>
                     <MediaQuery maxWidth={600}>
@@ -78,7 +111,7 @@ class IntroOfLinkU extends React.Component {
                             <b>링쿠</b>는 다같이 <b>먹고 놀고 친해지는</b> <br/>
                             <b>대학생 밥모임</b> 연결 서비스입니다. <br/>
                         </div>
-                        <Button style={{backgroundColor: '#5fa1d7', width: '150px', fontSize: '16px'}} color='blue' content='이용안내'
+                        <Button style={{backgroundColor: '#5fa1d7', marginTop: '30px', width: '150px', fontSize: '16px'}} color='blue' content='이용안내'
                             onClick={() => this.props.history.push('/about')} />
                     </MediaQuery>
                 </MediaQuery>
@@ -87,5 +120,10 @@ class IntroOfLinkU extends React.Component {
     }
 
 }
+const mapStateToProps = (state) => {
+    return {
+        loggedIn: state.login.loggedIn,
+    }
+};
 
-export default withRouter(IntroOfLinkU);
+export default withRouter(connect(mapStateToProps, {logout})(IntroOfLinkU));
