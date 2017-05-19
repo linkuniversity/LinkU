fe4d936895b60d9d455bd88c38038af8617f15f9
TWpower <elld8850@naver.com>
2017-05-19 17:50:47 +0900

    Change text in Sign Up Selection page

--- linku_frontend/linku/src/components/signup/SignupSelection.js
+++ linku_frontend/linku/src/components/signup/SignupSelection.js
@@ -26,9 +26,9 @@ class SignupSelection extends Component {
         return (
             <Container text>
                 <Button style={buttonStyle} onClick={() => {this.props.history.push('/signup')}}  fluid>학교메일로 인증하기</Button>
-                <Button style={buttonStyle} href="http://plus.kakao.com/home/@linku" fluid>학생증으로 인증하기</Button>
+                <Button style={buttonStyle} href="http://plus.kakao.com/home/@linku" fluid>링쿠 카톡 플러스친구로 학생증 보내서 인증하기</Button>
                 <p style={{fontSize: "17px", color: "#60a2d9", textAlign: "center"}}>
-                    ※ 대학생 이메일로 인증이 어려우신 분은 "학생증으로 인증하기"를 통해 <br/>
+                    ※ 대학생 이메일로 인증이 어려우신 분은 "링쿠 카톡 플러스친구로 학생증 보내서 인증하기"를 통해 <br/>
                     학생증 사진을 보내주시는 것으로도 가입이 가능해요 :D
                 </p>
             </Container>
